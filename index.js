6c25aa510945f0fb6fbc32c0deb6ed78bd4720d6
qingzhizhi <540548050@qq.com>
2020-12-23 16:23:22 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const http = require('http');
-
+const crypto = require('crypto');
+const secret = 'qq540548050';
+const {spawn} = require('child_process');
+function getCrypto(body) {
+  return 'sha1=' + crypto.createHmac('sha1', secret).update(body).digest('hex');
+}
 http
   .createServer((req, res) => {
-    console.log('mount');
-    console.log(req);
     //监听github接口调用
-    if (req.method === 'POST' && req.url === 'webhook') {
-      console.log(req);
-      console.log('enter');
+    if (req.method === 'POST' && req.url === '/webhook') {
+      let buffer = [];
+      req.on('data', (data) => {
+        buffer.push(data);
+      });
+      req.on('end', () => {
+        const body = Buffer.concat(buffer);
+        const event = req.headers['x-github-event'];
+        const sign = req.headers['x-hub-signature'];
+        if (sign !== getCrypto(body)) {
+          res.end('Not Allowed');
+          return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({code: 0}));
+        if (event === 'push') {
+          let payload = JSON.parse(body);
+          console.log(payload);
+          // spawn('sh',)
+        }
+      });
     }
     res.end('ok');
   })
