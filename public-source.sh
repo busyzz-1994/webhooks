11d9fe873f5de785e30d0414fac67cc351ab13c6
qingzhizhi <540548050@qq.com>
2020-12-23 22:40:35 +0800

    update

--- public-source.sh
+++ public-source.sh
@@ -1,8 +1,4 @@
-###
- # @Author: busyzz
- # @Date: 2020-12-23 16:33:10
- # @Description:
-###
+
 #!/bin/bash
 DIR=/project/public-source
 cd $DIR
@@ -11,4 +7,10 @@ git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
 git pull origin master
-
+echo "构建镜像"
+docker build -t public-source:1.0.0 .
+echo "删除旧容器"
+docker stop public-source-container
+docker rm public-source-container
+echo "启动容器"
+docker run -d -p 4000:4000 --name public-source-container public-source:1.0.0
